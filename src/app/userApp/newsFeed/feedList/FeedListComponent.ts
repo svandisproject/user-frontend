6b412c5b9ba5a1064031c83b298a70a026809edf
codeforges <ruben@codeforges.com>
2018-05-19 23:52:26 +0200

    fixed feed list styles ( top margin )

--- src/app/userApp/newsFeed/feedList/FeedListComponent.ts
+++ src/app/userApp/newsFeed/feedList/FeedListComponent.ts
@@ -9,6 +9,7 @@ import {Pageable} from '../../../common/api/dataModels/pageable/Pageable';
 @Component({
     selector: 'app-feed-list',
     templateUrl: './feedList.html',
+    styles: ['app-feed-list {margin-top: 15px; display: block}'],
     encapsulation: ViewEncapsulation.None
 })
 export class FeedListComponent {
