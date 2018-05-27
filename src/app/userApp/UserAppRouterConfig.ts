4b8818027f02b7c6f0de441848f530a9ad03a673
codeforges <ruben@codeforges.com>
2018-05-27 10:04:49 +0200

    Temporally removed default route for userApp and fixed layout

--- src/app/userApp/UserAppRouterConfig.ts
+++ src/app/userApp/UserAppRouterConfig.ts
@@ -4,11 +4,15 @@ import {LoginPageComponent} from '../common/pages/login/LoginPageComponent';
 import {AuthGuard} from '../common/guards/AuthGuard';
 import {IcoScreenerComponent} from './screener/ico/IcoScreenerComponent';
 import {AltCoinScreenerComponent} from './screener/altCoin/AltCoinScreenerComponent';
+import {UserAppComponent} from './UserAppComponent';
 
 export const UserAppRouterConfig: Route[] = [
-    {path: '', redirectTo: '/news-feed', pathMatch: 'full'},
-    {path: 'news-feed', component: NewsFeedComponent, canActivate: [AuthGuard]},
-    {path: 'ico', component: IcoScreenerComponent, canActivate: [AuthGuard]},
-    {path: 'alt-coins', component: AltCoinScreenerComponent, canActivate: [AuthGuard]},
-    {path: 'login', component: LoginPageComponent}
+    {
+        path: '', component: UserAppComponent, children: [
+            {path: 'news-feed', component: NewsFeedComponent, canActivate: [AuthGuard]},
+            {path: 'ico', component: IcoScreenerComponent, canActivate: [AuthGuard]},
+            {path: 'alt-coins', component: AltCoinScreenerComponent, canActivate: [AuthGuard]},
+            {path: 'login', component: LoginPageComponent}
+        ]
+    },
 ];
