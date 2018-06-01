1a4ce7572c25f198c1a688ac1a70bef795c6fe3d
codeforges <ruben@codeforges.com>
2018-06-01 18:03:17 +0200

    fixed nav config

--- src/app/userApp/config/FrontNavigation.ts
+++ src/app/userApp/config/FrontNavigation.ts
@@ -2,11 +2,11 @@ import {NavigationItem} from '../navigation/NavigationItem';
 
 export const FrontNavigation: NavigationItem[] = [
     // {cssClass: 'dashboard', path: '#', text: 'NAVIGATION.FRONT.ITEM.DASHBOARD'},
-    {cssClass: 'iso', path: 'user/ico', text: 'NAVIGATION.FRONT.ITEM.ICO_SCR'},
-    {cssClass: 'alt', path: 'user/alt-coins', text: 'NAVIGATION.FRONT.ITEM.ALT_COIN'},
+    {cssClass: 'iso', path: 'ico', text: 'NAVIGATION.FRONT.ITEM.ICO_SCR'},
+    {cssClass: 'alt', path: 'alt-coins', text: 'NAVIGATION.FRONT.ITEM.ALT_COIN'},
     // {cssClass: 'index', path: '#', text: 'NAVIGATION.FRONT.ITEM.INDEX_BUILDER'},
     // {cssClass: 'visualization', path: '#', text: 'NAVIGATION.FRONT.ITEM.VIS_TOOLS'},
     // {cssClass: 'market', path: '#', text: 'NAVIGATION.FRONT.ITEM.MARKET_CAPS'},
     // {cssClass: 'data', path: '#', text: 'NAVIGATION.FRONT.ITEM.DATA_MARKET'},
-    {cssClass: 'newsfeed', path: 'user/news-feed', text: 'NAVIGATION.FRONT.ITEM.NEWS'},
+    {cssClass: 'newsfeed', path: 'news-feed', text: 'NAVIGATION.FRONT.ITEM.NEWS'},
 ];
