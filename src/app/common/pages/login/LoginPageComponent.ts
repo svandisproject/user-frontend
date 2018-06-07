c34ad00ecadf1796c53bd24292a5d3f0911b8403
codeforges <ruben@codeforges.com>
2018-06-07 12:27:35 +0200

    login form validation errors

--- src/app/common/pages/login/LoginPageComponent.ts
+++ src/app/common/pages/login/LoginPageComponent.ts
@@ -1,4 +1,4 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, ViewChild, ViewEncapsulation} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {UserService} from '../../user/UserService';
 import {Router} from '@angular/router';
@@ -13,8 +13,12 @@ import {Router} from '@angular/router';
 export class LoginPageComponent {
     public email: string;
     public password: string;
+    public credsInvalid = false;
 
-    constructor(private userService: UserService, private router: Router) {
+    @ViewChild('form') form: NgForm;
+
+    constructor(private userService: UserService,
+                private router: Router) {
     }
 
     public login(form: NgForm) {
@@ -22,7 +26,10 @@ export class LoginPageComponent {
             this.userService.signIn({
                 username: this.email,
                 password: this.password
-            }).subscribe(res => this.router.navigate(['']));
+            }).subscribe(
+                res => this.router.navigate(['']),
+                err => this.credsInvalid = true
+            );
         }
     }
 }
