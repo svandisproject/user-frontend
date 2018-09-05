1298cf6e801a39a1c9110e7e5791f4845ca535b2
codeforges <ruben@codeforges.com>
2018-09-05 06:22:40 +0200

    refactoring and type definitions

---
+++ src/app/common/web3/Web3.ts
@@ -0,0 +1,18 @@
+// TODO: Extend and improve while adding or working with class definition
+export declare class Web3 {
+    eth: {
+        accounts: {
+            wallet: {
+                clear(): any
+                encrypt(...args): any
+                create(...args): any
+            }
+
+            decrypt(...args): any
+            sign(...args): any
+            recover(...args): any
+        }
+    };
+
+    constructor(args?: any)
+}
--- src/app/common/web3/Web3Service.ts
+++ src/app/common/web3/Web3Service.ts
@@ -1,67 +1,78 @@
-// Code originally references
-// https://github.com/FundRequest/code-snippets/edit/master/angular-smartcontracts/src/app/services/contracts/contracts.service.ts
-
-import {Injectable, OnInit} from '@angular/core';
-
-import Web3 from 'web3';
-import {WorkerService} from '../api/services/WorkerService';
-
-declare let require: any;
-declare let window: any;
+import {Injectable} from '@angular/core';
+import {Web3} from './Web3';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Web3Service {
-    private _account: string = null;
-    private _web3: any;
+    private web3: Web3;
 
-    private _password = 'Svandis1';
+    private password = 'Svandis1';
 
-    private _SIGN_NEW_USER = 'CREATE NEW ACCOUNT';
-    private _SIGN_NEW_SVANDIS_DATA = '{"token": "SVN", "supply": "400000000"}';
-    private _ENCRYPTED_PRV_KEY = 'ENCRYPTED_PRV_KEY';
+    private readonly SIGN_NEW_USER = 'CREATE NEW ACCOUNT';
+    private readonly SIGN_NEW_SVANDIS_DATA = JSON.stringify({
+        token: 'SVN',
+        supply: 400000000
+    });
+
+    private readonly ENCRYPTED_PRV_KEY = 'ENCRYPTED_PRV_KEY';
 
     constructor() {
-        this._web3 = new Web3('ws://localhost:9545');
-        // Clean the wallet and pull it/create it from an encrypted local source
-        this._web3.eth.accounts.wallet.clear();
-        // localStorage.removeItem(this._ENCRYPTED_PRV_KEY); // use to clear local accounts on localhost
-        const privateKeyEncrypted = localStorage.getItem(this._ENCRYPTED_PRV_KEY);
-        if (privateKeyEncrypted === null) {
-            this._web3.eth.accounts.wallet.create(1);
-            const encryptedPrivateKey = this._web3.eth.accounts.wallet.encrypt(this._password);
-            console.log(encryptedPrivateKey)
-            const walletString = JSON.stringify(encryptedPrivateKey[0]);
-            console.log('wallet string ' + walletString);
-            localStorage.setItem(this._ENCRYPTED_PRV_KEY, walletString);
+        this.web3 = new Web3('ws://localhost:9545');
+
+        this.clearWallet();
+        const privateKeyEncrypted = this.getKeyEncrypted();
+
+        if (!privateKeyEncrypted) {
+            this.createWalletAndStoreKey();
         } else {
-            this._web3.eth.accounts.decrypt(privateKeyEncrypted, this._password);
+            this.decryptKey(privateKeyEncrypted);
         }
     }
 
     public async signNewUser(): Promise<string> {
-        return Promise.resolve(this.signData(this._SIGN_NEW_USER));
+        return Promise.resolve(this.signData(this.SIGN_NEW_USER));
     }
 
     public async signSvandisData(): Promise<string> {
-        return Promise.resolve(this.signData(this._SIGN_NEW_SVANDIS_DATA));
+        return Promise.resolve(this.signData(this.SIGN_NEW_SVANDIS_DATA));
     }
 
+    // TODO: Refactor this method as i did with constructor
     public async signData(dataString: string): Promise<string> {
-        const privateKeyEncrypted = localStorage.getItem(this._ENCRYPTED_PRV_KEY);
-        const privateKey = this._web3.eth.accounts.decrypt( privateKeyEncrypted , this._password);
-        const signed = this._web3.eth.accounts.sign(
+        const privateKeyEncrypted = localStorage.getItem(this.ENCRYPTED_PRV_KEY);
+        const privateKey = this.web3.eth.accounts.decrypt(privateKeyEncrypted, this.password);
+        const signed = this.web3.eth.accounts.sign(
             dataString, privateKey.privateKey);
         const signature = signed.signature;
         const hash = signed.messageHash;
         // Lets prove we can get back our address
-        const sigRecovery = this._web3.eth.accounts.recover(dataString, signature);
+        const sigRecovery = this.web3.eth.accounts.recover(dataString, signature);
         console.log('Sig recovery is ' + sigRecovery + ' and the public key is ' +
             privateKey.address);
         if (sigRecovery === privateKey.address) {
             return Promise.resolve(signature);
         }
         return null;
+    }
+
+
+    private createWalletAndStoreKey() {
+        this.web3.eth.accounts.wallet.create(1);
+        const encryptedPrivateKey = this.web3.eth.accounts.wallet.encrypt(this.password);
+        const walletString = JSON.stringify(encryptedPrivateKey[0]);
+        localStorage.setItem(this.ENCRYPTED_PRV_KEY, walletString);
+    }
+
+    private decryptKey(privateKeyEncrypted) {
+        this.web3.eth.accounts.decrypt(privateKeyEncrypted, this.password);
+    }
+
+    private getKeyEncrypted() {
+        return localStorage.getItem(this.ENCRYPTED_PRV_KEY);
+    }
+
+    private clearWallet() {
+        this.web3.eth.accounts.wallet.clear();
     }
 }
