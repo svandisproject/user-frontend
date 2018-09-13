62781cd48809b1511e2c8f756f1c773fd56b184b
codeforges <ruben@codeforges.com>
2018-09-13 22:11:20 +0200

    mobile fix

--- src/app/userApp/UserAppComponent.ts
+++ src/app/userApp/UserAppComponent.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {AuthService} from '../common/auth/AuthService';
 import {Router} from '@angular/router';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 @Component({
     selector: 'app-user-app',
@@ -9,9 +10,20 @@ import {Router} from '@angular/router';
 })
 
 export class UserAppComponent {
-    constructor(private authService: AuthService, private router: Router) {
+    public isMobile = false;
+    public sidebarOpen = false;
+
+    constructor(private authService: AuthService,
+                private router: Router,
+                private breakpointObserver: BreakpointObserver) {
+        breakpointObserver.observe([Breakpoints.Tablet, Breakpoints.Small, Breakpoints.XSmall, Breakpoints.Handset])
+            .subscribe((result) => this.isMobile = result.matches);
     }
 
+    openSidebar() {
+        this.sidebarOpen = !this.sidebarOpen;
+        console.log(this.sidebarOpen);
+    }
 
     public isAuthenticated(): boolean {
         return this.authService.isAuthenticated();
--- src/app/userApp/sidebar/UserSidebarComponent.ts
+++ src/app/userApp/sidebar/UserSidebarComponent.ts
@@ -1,9 +1,10 @@
-import {Component, ViewEncapsulation} from '@angular/core';
+import {Component, Input, ViewEncapsulation} from '@angular/core';
 import {MenuItem, MenuItems} from './MenuItems';
 import {IpcService} from '../../common/electron/IpcService';
 import {WorkerService} from '../../common/api/services/WorkerService';
 import {Observable} from 'rxjs';
 import * as _ from 'lodash';
+import {BreakpointObserver, Breakpoints} from '@angular/cdk/layout';
 
 
 @Component({
@@ -13,13 +14,20 @@ import * as _ from 'lodash';
     encapsulation: ViewEncapsulation.None
 })
 export class UserSidebarComponent {
+    @Input() opened = false;
+
     public items: MenuItem[] = MenuItems;
+    public isMobile = false;
 
     constructor(private ipcService: IpcService,
+                private breakpointObserver: BreakpointObserver,
                 private workerService: WorkerService) {
         if (this.ipcService.isInitialized() && !_.find(this.items, { title: 'NAVIGATION.FRONT.ITEM.DATA_MINING_APP' }) ) {
             this.items.push({icon: 'stars', link: 'data-mining-app', title: 'NAVIGATION.FRONT.ITEM.DATA_MINING_APP'});
         }
+
+        breakpointObserver.observe([Breakpoints.Tablet, Breakpoints.Small, Breakpoints.XSmall, Breakpoints.Handset])
+            .subscribe((result) => this.isMobile = result.matches);
     }
 
     public isWorkerRunning(): Observable<boolean> {
