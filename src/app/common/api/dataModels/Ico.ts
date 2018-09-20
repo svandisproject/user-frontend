5827a711affb1f769ea9053a3b27cace32559724
codeforges <ruben@codeforges.com>
2018-09-20 23:51:54 +0200

    fix build

--- src/app/common/api/dataModels/Ico.ts
+++ src/app/common/api/dataModels/Ico.ts
@@ -39,22 +39,3 @@ export interface NewIco {
     team: Array<{ name: string, title: string }>;
     title: string;
 }
-
-// asset: {id: 19385, ticker: "XAL", price: 2.5, posts: [], convertable: false}
-// convertable: false
-// id: 19385
-// posts: []
-// price: 2.5
-// ticker: "XAL"
-// competitors: []
-// country: "EE"
-// dates: {}
-// description: "Auxledger enables organizations to build their fully customizable network as per business & compliance requirements. The multi-tiered networks deployed on Auxledger public network ensures data integrity across the ecosystem while maintaining interoperability with other networks using Auxledger decentralize consensus protocol."
-// finance: {token_price_eth: 0, accepted_currencies: ["ETH", "LTC", "BTC", "XRP", "LTC", "Fiat"],…}
-// industries: [{title: "Infrastructure", icos: []}]
-// links: {twitter: "https://twitter.com/auxledger/", slack: "",…}
-// partners: []
-// remote_id: 4855
-// restricted_countries: ["US"]
-// team: [{name: "Prof. Rudolf Mauch", title: "Blockchain Advisor",…},…]
-// title: "Auxledger"
