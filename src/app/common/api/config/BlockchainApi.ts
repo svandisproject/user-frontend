9408dd7f0f31aadacfd9fb5f78dcd5e8cdaa5b10
Victor Wiebe <35141155+victorwiebe@users.noreply.github.com>
2018-11-06 19:35:05 -0300

    Update BlockchainApi.ts

--- src/app/common/api/config/BlockchainApi.ts
+++ src/app/common/api/config/BlockchainApi.ts
@@ -1,5 +1,5 @@
 export class BlockchainApi {
-    public static readonly API_HOST = 'http://localhost:3000'
+    public static readonly API_HOST = 'http://localhost:3000';
     public static readonly API_VERSION = '';
     public static readonly API_URL = BlockchainApi.API_HOST  + BlockchainApi.API_VERSION;
 }
