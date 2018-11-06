93edb5be55aabcceb3dca372957e7f229ec6bd68
Ruben M <ruben@codeforges.com>
2018-11-06 19:51:45 +0100

    Update PostDetailsComponent.ts

--- src/app/userApp/newsFeed/postDetails/PostDetailsComponent.ts
+++ src/app/userApp/newsFeed/postDetails/PostDetailsComponent.ts
@@ -30,7 +30,6 @@ export class PostDetailsComponent implements OnInit {
     }
 
     public isAdmin(): boolean {
-        console.log(this.userAuthService.hasRoleAdmin());
         return this.userAuthService.hasRoleAdmin();
     }
 
