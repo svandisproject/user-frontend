13148a4bb8afeb2e600d17d7eee1d907d4b9ee4d
codeforges <ruben@codeforges.com>
2018-11-14 10:07:30 +0100

    trash

--- src/app/userApp/newsFeed/NewsFeedComponent.ts
+++ src/app/userApp/newsFeed/NewsFeedComponent.ts
@@ -130,7 +130,7 @@ export class NewsFeedComponent {
                 factory.add(new Filter('lk', filterItem.property, filterItem.value));
             }
         });
-
+        factory.add(new Filter('ne', 'tags.title', 'Trash'));
         return factory.build();
     }
 }
