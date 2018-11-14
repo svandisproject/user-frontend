f9e2cda20c40e549e2ccd38275723d54d0ad4a37
codeforges <ruben@codeforges.com>
2018-11-14 10:02:11 +0100

    trash

--- src/app/common/api/dataModels/Filter.ts
+++ src/app/common/api/dataModels/Filter.ts
@@ -4,16 +4,18 @@ export class Filter {
      * gt = greater then
      * lk = like
      * bw = between
+     * ne = not equals
      * in = in use 1 filter with this type , the value gets an array of string
      */
-    public type: 'eq' | 'gt' | 'lt' | 'lk' | 'bw' | 'in';
+    public type: 'eq' | 'gt' | 'lt' | 'lk' | 'bw' | 'in' | 'ne';
     public property: string;
     public value: string | string[];
 
 
-    constructor(type: 'eq' | 'gt' | 'lt' | 'lk' | 'bw' | 'in', property: string, value: string | string[]) {
+    constructor(type: 'eq' | 'gt' | 'lt' | 'lk' | 'bw' | 'in' | 'ne', property: string, value: string | string[]) {
         this.type = type;
         this.property = property;
         this.value = value;
     }
 }
+
