b2b226a5df68232c03fa99987320c5f095f6c5e0
codeforges <ruben@codeforges.com>
2018-11-16 18:00:50 +0100

    toolbar navigation SVN-99

--- src/app/userApp/toolbar/MenuItems.ts
+++ src/app/userApp/toolbar/MenuItems.ts
@@ -1,7 +1,7 @@
 export const MenuItems: MenuItem[] = [
+    {icon: 'library_books', link: 'news-feed', title: 'NAVIGATION.FRONT.ITEM.NEWS'},
     {icon: 'assessment', link: 'ico', title: 'NAVIGATION.FRONT.ITEM.ICO'},
-    {icon: 'copyright', link: 'alt-coins', title: 'NAVIGATION.FRONT.ITEM.ALT_COIN'},
-    {icon: 'library_books', link: 'news-feed', title: 'NAVIGATION.FRONT.ITEM.NEWS'}
+    {icon: 'copyright', link: 'alt-coins', title: 'NAVIGATION.FRONT.ITEM.ALT_COIN'}
 ];
 
 export interface MenuItem {
