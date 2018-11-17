c33ef565404582d093da7886fa7da539e5ad5158
codeforges <ruben@codeforges.com>
2018-11-17 09:29:25 +0100

    Filters on side nav

--- src/app/common/api/services/TagGroupService.ts
+++ src/app/common/api/services/TagGroupService.ts
@@ -4,9 +4,12 @@ import {Pageable} from '../dataModels/pageable/Pageable';
 import {Filter} from '../dataModels/Filter';
 import {TagGroupResource} from '../resource/TagGroupResource';
 import {EditTagGroup, TagGroup} from '../dataModels/TagGroup';
+import {BehaviorSubject} from 'rxjs';
+import {share} from 'rxjs/operators';
 
 @Injectable()
 export class TagGroupService {
+    private filterSelectSubject = new BehaviorSubject<Filter[]>(null);
 
     constructor(private tagGroupResource: TagGroupResource) {
     }
@@ -19,6 +22,18 @@ export class TagGroupService {
         return this.findBy([
             new Filter('eq', 'enabled', 'true'),
         ]);
+    }
+
+    public emitChange(filters: Filter[]) {
+        this.filterSelectSubject.next(filters);
+    }
+
+    public onFilterChange(): Observable<Filter[]> {
+        return this.filterSelectSubject.asObservable().pipe(share());
+    }
+
+    public getSelectedFitlers(): Filter[] {
+        return this.filterSelectSubject.getValue();
     }
 
     public create(tagGroup: EditTagGroup) {
