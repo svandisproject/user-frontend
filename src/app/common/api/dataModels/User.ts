e3f419f467719f582abd71148cd0d41f3c725ded
Victor Wiebe <victorw@svandis.io>
2018-12-06 05:21:15 -0800

    Add blockchain fields to the user frontend

--- src/app/common/api/dataModels/User.ts
+++ src/app/common/api/dataModels/User.ts
@@ -4,4 +4,9 @@ export interface User {
     id?: string;
     likedArticles?: string[];
     added_tag?: Tag[];
+    onboarded?: boolean;
+    centralized?: boolean;
+    key_addresses?: string[];
+    recovery_addresses?: string[];
+    identity_address?: string;
 }
