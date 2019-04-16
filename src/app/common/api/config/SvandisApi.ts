8f6e4ce3a1c3ea441ca8ae173bd06bd912f940e5
herman <herman@codeforges.com>
2019-04-16 12:14:27 +0200

    Change posts resources requests

--- src/app/common/api/config/SvandisApi.ts
+++ src/app/common/api/config/SvandisApi.ts
@@ -2,4 +2,5 @@ export class SvandisApi {
     public static readonly API_HOST = 'https://svandis-api-prod.herokuapp.com';
     public static readonly API_VERSION = '';
     public static readonly API_URL = SvandisApi.API_HOST + '/api' + SvandisApi.API_VERSION;
+    public static readonly ALT_API_URL = 'https://sv-node-api.herokuapp.com/api';
 }
