787b7c808fa0f130d61faee1bf6015ec94e601f1
hrmanlit <39740674+hrmanlit@users.noreply.github.com>
2019-04-16 17:36:43 +0300

    Change posts resources requests (#84)

--- src/app/common/api/resource/PostResource.ts
+++ src/app/common/api/resource/PostResource.ts
@@ -5,6 +5,9 @@ import {HttpService} from '../../http/HttpService';
 import {AbstractCrudResource} from '../util/AbstractCrudResource';
 import {Observable} from 'rxjs';
 import {Pageable} from '../dataModels/pageable/Pageable';
+import {Filter} from '../dataModels/Filter';
+import {Sorting} from '../util/Sorting';
+import * as _ from 'lodash';
 
 @Injectable()
 export class PostResource extends AbstractCrudResource<Post> {
@@ -14,5 +17,28 @@ export class PostResource extends AbstractCrudResource<Post> {
 
     public findMostLiked(period: string): Observable<Pageable<Post>> {
         return this.httpService.get(this.URL + '/most-liked/' + period);
+    }
+
+    public findBy(filters: Filter[], page: string = '1', sort?: Sorting, perPage?: number): Observable<Pageable<Post>> {
+        const encodedFilters: string = btoa(JSON.stringify(filters));
+        const params = {
+            filter: encodedFilters,
+            page: page
+        };
+
+        if (!_.get(sort, 'sort')) {
+            params['direction'] = 'desc';
+        } else {
+            params['sort'] = sort.sort;
+            params['direction'] = sort.direction || 'desc';
+        }
+
+        if (perPage) {
+            params['per_page'] = perPage;
+        }
+
+        return this.httpService.get(SvandisApi.ALT_API_URL + '/post' + '/filter', {
+            params: params
+        });
     }
 }
