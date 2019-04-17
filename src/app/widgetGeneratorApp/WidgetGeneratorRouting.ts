a8a4a4a3509ceb90432f520fad8337a4ebca8edc
OlegUshakov <r3ady2di3@mail.ru>
2019-04-17 03:36:27 +0300

    AuthGuard back

--- src/app/widgetGeneratorApp/WidgetGeneratorRouting.ts
+++ src/app/widgetGeneratorApp/WidgetGeneratorRouting.ts
@@ -3,6 +3,5 @@ import {GeneratorComponent} from './generator/GeneratorComponent';
 import {AuthGuard} from '../common/guards/AuthGuard';
 
 export const WidgetGeneratorRouting: Route[] = [
-    /*{path: '', component: GeneratorComponent, canActivate: [AuthGuard]}*/
-    {path: '', component: GeneratorComponent}
+    {path: '', component: GeneratorComponent, canActivate: [AuthGuard]}
 ];
